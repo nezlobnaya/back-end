191cc912b16cab6b782de02eda47053c837cc0ab
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2020-01-29 14:18:49 -0500

    db modeled, migration, seeds run

---
+++ data/migrations/20200129140952_stories.js
@@ -0,0 +1,49 @@
+
+exports.up = function(knex) {
+    return knex.schema
+    .createTable('users', tbl => {
+      tbl.increments()
+      tbl.string('username', 128).notNullable().unique()
+      tbl.string('password', 255).notNullable()
+      tbl.string('role').defaultTo('user')
+  })
+      .createTable('stories' , tbl => {
+        tbl.increments()
+        tbl.string('title', 128 ).notNullable()
+        tbl.text('contents').notNullable()
+        tbl.string('name')
+        tbl.string('email')
+        tbl.boolean('pending').notNullable()
+        tbl.integer('user_id')
+          .unsigned()
+          .references('id')
+          .inTable('users')
+          .onUpdate('CASCADE')
+          .onDelete('CASCADE')
+    })
+      .createTable('comments', tbl => {
+          tbl.increments()
+          tbl.text('contents').notNullable()
+          tbl.integer('story_id')
+              .unsigned()
+              .notNullable()
+              .references('id')
+              .inTable('stories')
+              .onUpdate('CASCADE')
+              .onDelete('CASCADE')
+          tbl.integer('user_id')
+              .unsigned()
+              .references('id')
+              .inTable('users')
+              .onUpdate('CASCADE')
+              .onDelete('CASCADE')
+      })
+    
+  };
+  
+  exports.down = function(knex) {
+    return knex.schema
+      .dropTableIfExists('comments')
+      .dropTableIfExists('stories')
+      .dropTableIfExists('users')  
+  };
--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,42 @@
-// Update with your config settings.
-
 module.exports = {
-
   development: {
     client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    connection: { filename: './data/refugees.db3' },
+    useNullAsDefault: true,
     pool: {
-      min: 2,
-      max: 10
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: './data/migrations',
+      tableName: 'dbmigrations',
+    },
+    seeds: { directory: './data/seeds' },
   },
-
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    client: 'sqlite3',
+    connection: { filename: './data/refugees.db3' },
+    useNullAsDefault: true,
     pool: {
-      min: 2,
-      max: 10
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
+      directory: './data/migrations',
+      tableName: 'dbmigrations',
+    },
+    seeds: { directory: './data/seeds' },
+  },
+  testing: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/test.db3',
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations',
+    },
+  },
 };
