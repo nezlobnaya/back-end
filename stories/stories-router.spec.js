92e8fc946b0497b2d70eea63733fe39bb0d66a4c
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2021-09-15 23:03:46 -0400

    fixed the memory leak in tests

--- stories/stories-router.spec.js
+++ stories/stories-router.spec.js
@@ -15,7 +15,7 @@ describe('Stories Router', () => {
         it('should return a status code of 200', () => {
             const expectedCode = 200
             let response
-            request(server).get('api/stories').then(res => {
+            request(server).get('/api/stories').then(res => {
                 response = res
                 expect(response.status).toBe(expectedCode)
                 expect(res.type).toBe('application/json')
